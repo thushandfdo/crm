3c838de91317215606006e29df4c246f6e0544bf
Thushan D. Fernando <thushandfdo@gmail.com>
2023-04-09 08:52:34 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+export default function App() {
+    return (
+        <div className='app'>
+            <h1 className='text-primary text-4xl font-bold'>React App</h1>
+            <p className="p1">Test 01</p>
+            <p className="p2">Test 02</p>
+        </div>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    target: 'web',
+    devServer: {
+        port: '3000',
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+};
